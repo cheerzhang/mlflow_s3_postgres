e10b5470617f903a6ae9a69b8a40c20e7e649a26
cheerzhang <zhanglenus@gmail.com>
2023-12-14 09:40:28 +0100

    add sh file

---
+++ start_mlflow.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Attempt MLflow database upgrade
+mlflow db upgrade --database-uri postgresql://postgres:postgres@mlflowdb1.ctrcoixorex7.eu-north-1.rds.amazonaws.com:5432/postgres
+
+# Check the exit code of the previous command
+if [ $? -eq 0 ]; then
+    # Database upgrade succeeded, start MLflow server
+    mlflow server --host 0.0.0.0 --port $MLFLOW_SERVER_PORT --backend-store-uri postgresql://postgres:postgres@mlflowdb1.ctrcoixorex7.eu-north-1.rds.amazonaws.com:5432/postgres --default-artifact-root s3://mlflow-s32/mlflow_arts/
+else
+    # Database upgrade failed, print an error message
+    echo "Failed to upgrade database. Exiting without starting MLflow server."
+    mlflow server --host 0.0.0.0 --port $MLFLOW_SERVER_PORT --backend-store-uri postgresql://postgres:postgres@mlflowdb1.ctrcoixorex7.eu-north-1.rds.amazonaws.com:5432/postgres --default-artifact-root s3://mlflow-s32/mlflow_arts/
+fi
+
